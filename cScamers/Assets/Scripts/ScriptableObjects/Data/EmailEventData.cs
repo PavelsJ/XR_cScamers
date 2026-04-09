207a3f8ebca35d7e2b6ed3348411d3be3adacd26
martiņš <martins.mikelsons7@gmail.com>
2026-04-09 10:42:31 +0300

    Changed Events

--- cScamers/Assets/Scripts/ScriptableObjects/Data/EmailEventData.cs
+++ cScamers/Assets/Scripts/ScriptableObjects/Data/EmailEventData.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EmailEventData", menuName = "Scriptable Objects/EmailEventData")]
 public class EmailEventData : EventData
 {
-    
+    [Header("Email Adress")]
+    [TextArea(2, 4)] public string adress;
+    public Sprite prophile;
 }
